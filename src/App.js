4516f322fe7eb2df54f442af737533e2b5d7824f
Mario Ramos <mario.ramos@inteli-k.com>
2021-07-07 00:37:55 -0500

    Freeagent challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import Login from './views/login/login.view';
+import Home from './views/home/home.view';
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route
+} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <Router>
+               <Switch>
+                  <Route exact path="/">
+                     <Login />
+                  </Route>
+                  <Route exact path="/user/:username/:token">
+                     <Home />
+                  </Route>
+               </Switch>
+         </Router>
+      </div>
+   );
 }
 
 export default App;
